943557991599f4cd7f55098859556f2e87010664
Slava Kuchenev <666sws66613@gmail.com>
2024-04-10 16:29:17 +0300

    Lesson_16

--- src/test/java/Api/PostmanEchoAPITest.java
+++ src/test/java/Api/PostmanEchoAPITest.java
@@ -1,40 +1,130 @@
 package Api;
 
-import io.restassured.RestAssured;
-import org.testng.annotations.BeforeClass;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
 
 public class PostmanEchoAPITest {
-
-    @BeforeClass
-    public void setup() {
-        RestAssured.baseURI = "https://postman-echo.com";
-    }
+    private final String URL = "https://postman-echo.com";
 
     @Test
-    public void testGETRequest() {
-        given()
-                .queryParam("foo1", "bar1")
-                .queryParam("foo2", "bar2")
+    public void testGetRequest() {
+        api.reqres.spec.Specifications.installSpecification(api.reqres.spec.Specifications.requestSpec(URL), api.reqres.spec.Specifications.responseSpecOK200());
+        Response response = given()
+                .body("foo1=bar1&foo2=bar2")
                 .when()
-                .get("/get")
-                .then()
-                .statusCode(200)
-                .body("args.foo1", equalTo("bar1"))
-                .body("args.foo2", equalTo("bar2"));
+                .get("/get?foo1=bar1&foo2=bar2")
+                .then().log().all()
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        assertEquals("443", jsonPath.get("headers.'x-forwarded-port'"));
+        assertEquals("postman-echo.com", jsonPath.get("headers.host"));
+        assertNotNull(jsonPath.get("headers.'x-amzn-trace-id'"));
+        assertEquals("19", jsonPath.get("headers.'content-length'"));
+        assertEquals("*/*", jsonPath.get("headers.accept"));
+        assertEquals("gzip,deflate", jsonPath.get("headers.'accept-encoding'"));
     }
 
     @Test
-    public void testPOSTRequest() {
-        given()
-                .header("Content-Type", "text/plain")
-                .body("{\"test\": \"value\"}")
+    public void testPostRawTextRequest() {
+        api.reqres.spec.Specifications.installSpecification(api.reqres.spec.Specifications.requestSpec(URL), api.reqres.spec.Specifications.responseSpecOK200());
+        Response response = given()
+                .body("This is expected to be sent back as part of response body.")
                 .when()
                 .post("/post")
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("{\"test\": \"value\"}"));
+                .then().log().all()
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        assertEquals("443", jsonPath.get("headers.'x-forwarded-port'"));
+        assertEquals("postman-echo.com", jsonPath.get("headers.host"));
+        assertNotNull(jsonPath.get("headers.'x-amzn-trace-id'"));
+        assertEquals("58", jsonPath.get("headers.'content-length'"));
+        assertEquals("*/*", jsonPath.get("headers.accept"));
+        assertEquals("gzip,deflate", jsonPath.get("headers.'accept-encoding'"));
+    }
+    @Test
+    public void testPostFromDataRequest() {
+        api.reqres.spec.Specifications.installSpecification(api.reqres.spec.Specifications.requestSpec(URL), api.reqres.spec.Specifications.responseSpecOK200());
+        Response response = given()
+                .body("foo1=bar1&foo2=bar2")
+                .when()
+                .post("/post")
+                .then().log().all()
+                .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        assertEquals("443", jsonPath.get("headers.'x-forwarded-port'"));
+        assertEquals("postman-echo.com", jsonPath.get("headers.host"));
+        assertNotNull(jsonPath.get("headers.'x-amzn-trace-id'"));
+        assertEquals("19", jsonPath.get("headers.'content-length'"));
+        assertEquals("*/*", jsonPath.get("headers.accept"));
+        assertEquals("gzip,deflate", jsonPath.get("headers.'accept-encoding'"));
+    }
+
+    @Test
+    public void testPutRequest() {
+        api.reqres.spec.Specifications.installSpecification(api.reqres.spec.Specifications.requestSpec(URL), api.reqres.spec.Specifications.responseSpecOK200());
+        Response response = given()
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .put("/put")
+                .then().log().all()
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        assertEquals("443", jsonPath.get("headers.x-forwarded-port"));
+        assertEquals("postman-echo.com", jsonPath.get("headers.host"));
+        assertNotNull(jsonPath.get("headers.x-amzn-trace-id"));
+        assertEquals("58", jsonPath.get("headers.content-length"));
+        assertEquals("*/*", jsonPath.get("headers.accept"));
+        assertEquals("gzip,deflate", jsonPath.get("headers.accept-encoding"));
+    }
+
+    @Test
+    public void testPatchRequest() {
+        api.reqres.spec.Specifications.installSpecification(api.reqres.spec.Specifications.requestSpec(URL), api.reqres.spec.Specifications.responseSpecOK200());
+        Response response = given()
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .patch("/patch")
+                .then().log().all()
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        assertEquals("443", jsonPath.get("headers.x-forwarded-port"));
+        assertEquals("postman-echo.com", jsonPath.get("headers.host"));
+        assertNotNull(jsonPath.get("headers.x-amzn-trace-id"));
+        assertEquals("58", jsonPath.get("headers.content-length"));
+        assertEquals("*/*", jsonPath.get("headers.accept"));
+        assertEquals("gzip,deflate", jsonPath.get("headers.accept-encoding"));
+    }
+
+    @Test
+    public void testDeleteRequestWithApiKey() {
+        api.reqres.spec.Specifications.installSpecification(api.reqres.spec.Specifications.requestSpec(URL), api.reqres.spec.Specifications.responseSpecOK200());
+        Response response = given()
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .delete("/delete")
+                .then().log().all()
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        assertEquals(response.getStatusCode(), 200);
+        assertEquals("This is expected to be sent back as part of response body.", jsonPath.get("data"));
+        assertEquals("443", jsonPath.get("headers.x-forwarded-port"));
+        assertEquals("postman-echo.com", jsonPath.get("headers.host"));
+        assertNotNull(jsonPath.get("headers.x-amzn-trace-id"));
+        assertEquals("58", jsonPath.get("headers.content-length"));
+        assertEquals("*/*", jsonPath.get("headers.accept"));
+        assertEquals("gzip,deflate", jsonPath.get("headers.accept-encoding"));
     }
 }
+
